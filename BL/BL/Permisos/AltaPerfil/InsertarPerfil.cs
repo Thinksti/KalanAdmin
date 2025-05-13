1738f3a17bf5b2bb01e3a8f5bf394cfa1c25850c
Gerardo Antonio Dzib Balam <gerardoantoniodb@gmail.com>
2025-05-13 17:43:49 -0600

    Agregando Carpetas para los respectivos archivos de Alta del Perfil
    - Se agregaron carpetas en BL y en Frontend para el apartado de Alta de los perfiles
    
    - Se agrego en el archivo de backend para insertar los perfiles en la tabla aspnetroles
    
    - se agrego el componente de razor para poder manejar el formulario para dar de alta el perfil

---
+++ BL/BL/Permisos/AltaPerfil/InsertarPerfil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Kalan.Contextos;
+
+namespace KalanBlazor.BL.Permisos.AltaPerfil
+{
+    public class InsertarPerfil
+    {
+
+        public static void Insertar(string cadenaConexion, string Name, string UsuarioCreacion)
+        {
+            using var context = new KalanDB(cadenaConexion);
+
+            var nuevoRol = new Shared.Kalan.Entidades.aspnetroles
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = Name,
+                Activo = true,
+                UsuarioCreacion = UsuarioCreacion,
+                FechaCreacion = DateTime.Now,
+                UsuarioModifica = null,
+                FechaModifica = DateTime.Now
+            };
+
+            context.aspnetroles.Add(nuevoRol);
+            context.SaveChanges();
+        }
+    }
+}
